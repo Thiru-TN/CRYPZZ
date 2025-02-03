6bffb20a265382806155e3c2fd2a111432edd191
ojas kittur <ojas.kittur2022@vitstudent.ac.in>
2025-02-03 17:49:09 +0530

    added signup functionality

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true })); 
 app.use(express.json());
 app.use(cookieParser()); 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "http://localhost:5174", credentials: true }));
 
 
 const PORT = process.env.PORT
